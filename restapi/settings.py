267f9c9fccf836115bd6eab9c5f9c8acc7c554ac
Antesh <anteshkumar2003@gmail.com>
2024-03-01 11:21:03 +0530

    Vercel Host configured

--- restapi/settings.py
+++ restapi/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&k^nm*01qxec$uq5ih!b@8_ehqz&*k-7&sz9j)1l%m6%pz4ak='
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
